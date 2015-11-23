b784c0ac3b32607b45fd286b643c08c904d82b9a
dylan-reeves <dylan.reeves@sabulk.co.za>
2015-11-23 21:37:37 +0200

    Creating group specific menus

--- departments/urls.py
+++ departments/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     #example of url /simpleplantms/departments/
-    url(r'^$', views.IndexView.as_view(), name='departments-index'),
+    url(r'^$', login_required(views.IndexView.as_view()), name='departments-index'),
     #example of url /simpleplantms/departments/20/
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='departments-details'),
+    url(r'^(?P<pk>[0-9]+)/$', login_required(views.DetailView.as_view()), name='departments-details'),
     #example of url /simpleplantms/departments/34/update/
-    url(r'^(?P<pk>[0-9]+)/update/$', views.UpdateView.as_view(), name='departments-update'),
+    url(r'^(?P<pk>[0-9]+)/update/$', login_required(views.UpdateView.as_view()), name='departments-update'),
     #example of url /simpleplantms/departments/34/delete/
-    url(r'^(?P<pk>[0-9]+)/delete/$', views.DeleteView.as_view(), name='departments-delete'),
+    url(r'^(?P<pk>[0-9]+)/delete/$', login_required(views.DeleteView.as_view()), name='departments-delete'),
     #example of url /simpleplantms/departments/create/
-    url(r'^create/', views.CreateView.as_view(), name='departments-create'),
+    url(r'^create/', login_required(views.CreateView.as_view()), name='departments-create'),
 ]
--- sites/admin.py
+++ sites/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
 
 from .models import site
 
 # Register your models here.
 admin.site.register(site)
+admin.site.register(Permission)
