ea4b00e8eb59b683118b9daad38efcb1a6a47c96
dylan-reeves <dylan.reeves@sabulk.co.za>
2015-12-24 10:50:47 +0200

    Creating dynamic execution form

--- maintenance/forms.py
+++ maintenance/forms.py
@@ -2,8 +2,28 @@ from django import forms
 
 from equipment.models import equipment, maintenanceschedule
 from .models import maintenancerecord, maintenancerecorddetails
-from
+from mainttask.models import MaintenanceJob, MaintenanceTaskDetailItems
 
-class RecordForm(forms.ModelForm):
-    class Meta:
-        model = maintenancerecord
+class RecordForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        print(kwargs.pop('my_arg'))
+        tasks = MaintenanceTaskDetailItems.objects.filter(pk=kwargs.pop('my_arg')).order_by('orderfield')
+        Completed = 'OK'
+        NotCompleted = 'NO'
+        NotApplicable = 'NA'
+        completed_choices = (
+            (Completed, 'OK'),
+            (NotCompleted, 'NO'),
+            (NotApplicable, 'NA'),
+            )
+        super(RecordForm, self).__init__(*args, **kwargs)
+        itemnumber=1
+        for task in tasks:
+            print(task.task)
+            self.fields["completed_%d" % itemnumber] = forms.ChoiceField(choices=completed_choices, label=task.task)
+            self.fields["comment_%d" % itemnumber] = forms.CharField()
+            itemnumber = itemnumber + 1
+
+    def printsuccess(self):
+        print('happiness')
+        pass
--- maintenance/views.py
+++ maintenance/views.py
@@ -6,10 +6,21 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
 
 from equipment.models import equipment, maintenanceschedule
+from .forms import RecordForm
 # Create your views here.
 class IndexView(generic.ListView):
     queryset = maintenanceschedule.objects.order_by('nextdate')[:5]
     template_name = 'maintenance/index.html'
     context_object_name = 'upcoming_maintenance'
 
-class
+class ExecuteView(generic.edit.FormView):
+    template_name = 'maintenance/execute.html'
+    success_url = '/maintenance/'
+    form_class = RecordForm
+    def dispatch(self, *args, **kwargs):
+        form_class = RecordForm(my_arg=self.kwargs['pk'])
+        return super(ExecuteView, self).dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        form.printsuccess()
+        return super(ExecuteView, self).form_valid(form)
